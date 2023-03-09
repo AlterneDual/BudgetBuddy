8bd2b88097f360aa89e14d6a51aa62bafd307753
Samz685 <94040038+Samz685@users.noreply.github.com>
2023-03-09 17:06:48 +0100

    splash con zoom

--- app/src/main/java/run/budgetbuddy/activities/splash/SplashActivity.kt
+++ app/src/main/java/run/budgetbuddy/activities/splash/SplashActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import run.budgetbuddy.R
 import run.budgetbuddy.activities.MG.MgInicio
 
@@ -12,15 +15,18 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Establece un temporizador para mostrar la actividad principal después de 3 segundos
-        val splashTimer = object : CountDownTimer(2000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {}
-            override fun onFinish() {
+        val imageView = findViewById<ImageView>(R.id.splash_logo_budget)
+        val animZoomIn = AnimationUtils.loadAnimation(applicationContext, R.anim.animation_splash)
+        animZoomIn.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                // Iniciar la actividad principal después de que finalice la animación
                 startActivity(Intent(this@SplashActivity, MgInicio::class.java))
                 finish()
             }
-        }
-        splashTimer.start()
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+        imageView.startAnimation(animZoomIn)
+    }
 
     }
-}
